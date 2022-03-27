e23406c60b5ce76a08c8c6e1f9f833b193cd0fb5
alfattahdicky <alfattahdicky2@gmail.com>
2022-03-27 19:42:43 +0700

    Learn useState

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [name, setName] = useState("dicky");
+  const [person, setPerson] = useState({ name: "diaz", age: 20 });
+
+  const handlerName = () => {
+    setName("azka");
+    setPerson({ name: "vera", age: 24 });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My name is {name}</Text>
+      <Text>
+        His name {person.name} and age {person.age}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={handlerName} />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    marginTop: 20,
   },
 });
+
+export default App;
